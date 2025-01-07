c862ed46864b3e83fc8c1f684b4650786646fa21
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-07 13:59:11 -0300

    Update main.ts

--- prism-erp-frontend/src/main.ts
+++ prism-erp-frontend/src/main.ts
@@ -1,6 +1,25 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './app/core/interceptors/auth.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+    // Outros provedores
+  ],
+}).catch((err) => console.error(err));
