76a028de7cf70f4aaea6ba12bfcd605d71758b9a
mathfrancisco2 <math.francisco2@gmail.com>
2025-01-07 13:16:13 -0300

    ATT

---
+++ prism-erp-frontend/src/app/features/dashboard/activity-summary/activity-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './activity-summary.component.html',
+  styleUrl: './activity-summary.component.scss'
+})
+export class ActivitySummaryComponent {
+
+}
---
+++ prism-erp-frontend/src/app/features/dashboard/sales-chart/sales-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-chart.component.html',
+  styleUrl: './sales-chart.component.scss'
+})
+export class SalesChartComponent {
+
+}
