5d03fbbd553a6c86a8b771ed74e13985e5c95444
mathfrancisco2 <math.francisco2@gmail.com>
2025-01-09 23:06:21 -0300

    att

--- prism-erp-frontend/src/app/app.config.ts
+++ prism-erp-frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {AuthInterceptor} from './core/interceptors/authinterceptor';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideHttpClient(withInterceptors([AuthInterceptor]), withFetch()),
+    provideAnimations()
     // Outros provedores
   ],
 };
