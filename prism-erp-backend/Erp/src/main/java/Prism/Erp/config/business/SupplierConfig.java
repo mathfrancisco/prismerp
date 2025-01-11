90a1f0e72f8be3ac5f2045d94e38dd5db32c6375
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2025-01-11 19:18:40 -0300

    Atualizar o SupplierConfig.java

--- prism-erp-backend/Erp/src/main/java/Prism/Erp/config/business/SupplierConfig.java
+++ prism-erp-backend/Erp/src/main/java/Prism/Erp/config/business/SupplierConfig.java
@@ -1,4 +1,19 @@
 package Prism.Erp.config.business;
 
+@Configuration
+@EnableScheduling
 public class SupplierConfig {
+
+    @Bean
+    public ScheduledExecutorService supplierScheduler() {
+        return Executors.newScheduledThreadPool(2);
+    }
+
+    @Bean
+    public RestTemplate supplierRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler());
+        return restTemplate;
+    }
 }
